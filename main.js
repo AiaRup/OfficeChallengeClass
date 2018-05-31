416160fc929d9c8d7fae2d9c546900ffcf51bddf
AiaRup <aiale88@gmail.com>
2018-05-31 11:06:45 +0300

    finished with all classes

--- main.js
+++ main.js
@@ -10,7 +10,66 @@ class Employee {
   constructor (employeeName) {
     this.name = employeeName;
   }
+
+  work (office) {
+    for (let i = 0; i < 10; i++) {
+      office.documents.push(new Document(this.name));
+    }
+  }
 }
+
+class Manager {
+  constructor (managerName) {
+    this.name = managerName;
+    this.employees = [];
+  }
+
+  hireEmployee (employeeName) {
+    let employee = new Employee(employeeName);
+    this.employees.push(employee);
+  }
+
+  askEmployeesToWork (office) {
+    this.employees.forEach((employee) => {
+      employee.work(office);
+    });
+  }
+}
+
+class Cleaner {
+  constructor (cleanerName) {
+    this.name = cleanerName;
+  }
+
+  clean () {
+    console.log('Clean');
+
+  }
+}
+
+class Office {
+  constructor () {
+    this.documents = [];
+    this.managers = [];
+    this.cleaners = [];
+  }
+
+  hireManager (managerName) {
+    this.managers.push(new Manager(managerName));
+  }
+
+  hireCleaner (cleanerName) {
+    this.cleaners.push(new Cleaner(cleanerName));
+  }
+
+  startWorkDay () {
+    this.managers.forEach(manager => {
+      manager.askEmployeesToWork(this);
+    });
+  }
+}
+
+
 
 
 
